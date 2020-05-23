de563322d595e1008c8a6890812de3ea3c670e6b
Dan Norris <protochron@users.noreply.github.com>
2020-05-23 14:41:36 -0400

    Add support for json output
    This adds a new flag to the `gen` command in `nk` called `--output`. It
    takes one of two parameters:
    * `text` (default): the current human-readable format
    * `json`: formats the key and seed in JSON so that they can be fed to
      other programs via stdin.
      The message is pretty straightforward:
    
      ```json
      {
        "private_key": "A...",
        "seed": "S..."
      }
      ```

--- src/bin/nk/main.rs
+++ src/bin/nk/main.rs
@@ -1,6 +1,12 @@
+extern crate serde_json;
+
 use nkeys::{self, KeyPair, KeyPairType};
-use structopt::StructOpt;
+use serde_json::json;
+use std::error::Error;
+use std::fmt;
+use std::str::FromStr;
 use structopt::clap::AppSettings;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt, Clone)]
 #[structopt(
@@ -17,10 +23,50 @@ struct Cli {
 enum Command {
     #[structopt(name = "gen", about = "Generates a key pair")]
     Gen {
-        /// The type of key pair to generate. May be Account, User, Module, Server, Operator, Cluster         
+        /// The type of key pair to generate. May be Account, User, Module, Server, Operator, Cluster
         #[structopt(case_insensitive = true)]
         keytype: KeyPairType,
+        #[structopt(
+            short = "o",
+            long = "output",
+            default_value = "text",
+            help = "Specify output format (text or json)"
+        )]
+        output: Output,
     },
+}
+
+#[derive(StructOpt, Debug, Clone)]
+enum Output {
+    Text,
+    JSON,
+}
+
+impl FromStr for Output {
+    type Err = OutputParseErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(Output::JSON),
+            "text" => Ok(Output::Text),
+            _ => Err(OutputParseErr),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct OutputParseErr;
+
+impl Error for OutputParseErr {}
+
+impl fmt::Display for OutputParseErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            "error parsing output type, see help for the list of accepted outputs"
+        )
+    }
 }
 
 fn main() {
@@ -29,17 +75,29 @@ fn main() {
     env_logger::init();
 
     match cmd {
-        Command::Gen { keytype } => {
-            generate(keytype);
+        Command::Gen { keytype, output } => {
+            generate(keytype, output);
         }
     }
 }
 
-fn generate(kt: &KeyPairType) {
+fn generate(kt: &KeyPairType, output_type: &Output) {
     let kp = KeyPair::new(kt.clone());
-    println!(
-        "Public Key: {}\nSeed: {}\n\nRemember that the seed is private, treat it as a secret.",
-        kp.public_key(),
-        kp.seed().unwrap()
-    );
+    match output_type {
+        Output::Text => {
+            println!(
+                "Public Key: {}\nSeed: {}\n\nRemember that the seed is private, treat it as a secret.",
+                kp.public_key(),
+                kp.seed().unwrap()
+            );
+        }
+        Output::JSON => {
+            let output = json!({
+                "public_key": kp.public_key(),
+                "seed": kp.seed().unwrap(),
+            });
+
+            println!("{}", output.to_string());
+        }
+    }
 }
